bcb7b2546b7808d90ba53228873f1ee4b952e444
Joonw00 <97094633+Joonw00@users.noreply.github.com>
2022-08-13 16:23:56 +0900

    8.13
    카카오 대충 완성, 함수형 일부 사용

--- kakao/lib/main.dart
+++ kakao/lib/main.dart
@@ -16,10 +16,88 @@ class MyApp extends StatelessWidget {
   }
 }
 
+dividLine(var boxheight, var dividerheight) {
+  return Column(
+    children: [
+      SizedBox(
+        height: boxheight,
+      ),
+      Divider(
+        height: dividerheight,
+        color: Colors.black,
+      )
+    ],
+  );
+}
+
+//세로로 프로필 설정
+columnFriend(var name) {
+  return Row(
+    children: [
+      Column(
+        children: <Widget>[
+          CircleAvatar(
+            radius: 20.0,
+            backgroundColor: Colors.blue,
+            child: Icon(
+              Icons.person,
+              size: 40,
+            ),
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          Text(
+            '$name',
+            style: TextStyle(
+              fontSize: 15,
+            ),
+          ),
+        ],
+      ),
+      SizedBox(
+        width: 15,
+      ),
+    ],
+  );
+}
+
+rowFriend(var name) {
+  return Column(
+    children: [
+      Row(
+        children: <Widget>[
+          CircleAvatar(
+            radius: 20.0,
+            backgroundColor: Colors.blue,
+            child: Icon(
+              Icons.person,
+              size: 40,
+            ),
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          Text(
+            '$name',
+            style: TextStyle(
+              fontSize: 15,
+            ),
+          ),
+        ],
+      ),
+      SizedBox(
+        height: 10,
+      ),
+    ],
+  );
+}
+
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: true,
       appBar: AppBar(
         title: Text(
           '친구',
@@ -96,10 +174,7 @@ class MyHomePage extends StatelessWidget {
                 ],
               ),
             ),
-            Divider(
-              height: 20,
-              color: Colors.black,
-            ),
+            dividLine(0.0, 20.0),
             SizedBox(
               height: 4,
             ),
@@ -109,11 +184,8 @@ class MyHomePage extends StatelessWidget {
                 fontSize: 10,
               ),
             ),
-            SizedBox(
-              height: 6,
-            ),
             Container(
-              height: 50,
+              height: 35,
               child: Row(
                 children: <Widget>[
                   Icon(
@@ -122,7 +194,7 @@ class MyHomePage extends StatelessWidget {
                     size: 30,
                   ),
                   SizedBox(
-                    width: 10,
+                    width: 5,
                   ),
                   Text(
                     '친구별로 다른 프로필을 설정해보세요!',
@@ -133,10 +205,7 @@ class MyHomePage extends StatelessWidget {
                 ],
               ),
             ),
-            Divider(
-              height: 20,
-              color: Colors.black,
-            ),
+            dividLine(0.0, 20.0),
             SizedBox(
               height: 4,
             ),
@@ -205,13 +274,7 @@ class MyHomePage extends StatelessWidget {
                 ],
               ),
             ),
-            SizedBox(
-              height: 40,
-            ),
-            Divider(
-              height: 20,
-              color: Colors.black,
-            ),
+            dividLine(40.0, 20.0),
             SizedBox(
               height: 4,
             ),
@@ -221,9 +284,97 @@ class MyHomePage extends StatelessWidget {
                 fontSize: 10,
               ),
             ),
+            SizedBox(
+              height: 6,
+            ),
+            Container(
+              height: 50,
+              child: Row(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      columnFriend('H0R'),
+                      columnFriend('박지수'),
+                      columnFriend('이채경'),
+                      columnFriend('임채은'),
+                      columnFriend('혜지'),
+                      columnFriend('이준한'),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            dividLine(20.0, 20.0),
+            Text(
+              '즐겨찾기',
+              style: TextStyle(
+                fontSize: 10,
+              ),
+            ),
+            SizedBox(
+              height: 6,
+            ),
+            Container(
+              height: 50,
+              child: Column(
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      rowFriend('최어진'),
+                      rowFriend('이성형'),
+                      rowFriend('신은하'),
+                    ],
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
+      bottomNavigationBar: BottomAppBar(
+          child: SizedBox(
+        height: 60,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            IconButton(
+              icon: Icon(
+                Icons.person,
+                color: Colors.black,
+              ),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(
+                Icons.chat_bubble_outline_outlined,
+                color: Colors.black,
+              ),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(
+                Icons.remove_red_eye_outlined,
+                color: Colors.black,
+              ),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(
+                Icons.shopping_bag_outlined,
+                color: Colors.black,
+              ),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(
+                Icons.more_horiz_outlined,
+                color: Colors.black,
+              ),
+              onPressed: () {},
+            ),
+          ],
+        ),
+      )),
     );
   }
 }
--- practice/lib/main.dart
+++ practice/lib/main.dart
@@ -1,19 +1,145 @@
 import 'package:flutter/material.dart';
-import 'ScreenA.dart';
-import 'ScreenB.dart';
-import 'ScreenC.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Navigation',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return const MaterialApp(home: ListViewPage());
+  }
+}
+
+class ListViewPage extends StatefulWidget {
+  const ListViewPage({Key? key}) : super(key: key);
+  @override
+  State<ListViewPage> createState() => _ListViewPageState();
+}
+
+class _ListViewPageState extends State<ListViewPage> {
+  var titleList = [
+    'lion profile',
+    'jump',
+    'stand',
+  ];
+  var imageList = [
+    'image/lion.jpg',
+    'image/jump.jpg',
+    'image/stand.jpg',
+  ];
+  var description = [
+    '1.There are different types of careers you can pursue in your life. Which one will it be?',
+    '2.There are different types of careers you can pursue in your life. Which one will it be?',
+    '3.There are different types of careers you can pursue in your life. Which one will it be?',
+  ];
+  void showPopup(context, title, image, description) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return Dialog(
+          child: Container(
+            width: MediaQuery.of(context).size.width * 0.7,
+            height: 380,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: Colors.white,
+            ),
+            child: Column(children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Image.asset(
+                  image,
+                  width: 200,
+                  height: 200,
+                ),
+              ),
+              const SizedBox(height: 10),
+              Text(
+                title,
+                style: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8),
+                child: Text(
+                  description,
+                  maxLines: 3,
+                  style: const TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              ElevatedButton.icon(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  icon: const Icon(Icons.close),
+                  label: const Text('Close'))
+            ]),
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width *
+        0.6; // 가로 모드 시 description이 디바이스의 60%차지
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'ListView',
+          style: TextStyle(color: Colors.grey),
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0,
       ),
-      home: ScreenA(),
+      body: ListView.builder(
+        //builder - 스크린에 보여지는 만큼만 불러옴
+        itemCount: titleList.length,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () {
+              showPopup(context, titleList[index], imageList[index],
+                  description[index]);
+            },
+            child: Card(
+                child: Row(
+              children: [
+                SizedBox(
+                  //크기만 정함, 데코x, const생성자를 가짐
+                  width: 100,
+                  height: 100,
+                  child: Image.asset(imageList[index]),
+                ),
+                Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Column(
+                      children: [
+                        Text(titleList[index],
+                            style: const TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold)),
+                        const SizedBox(height: 10),
+                        SizedBox(
+                          width: width,
+                          child: Text(description[index],
+                              style: const TextStyle(
+                                  fontSize: 15,
+                                  color: Colors.grey,
+                                  fontWeight: FontWeight.w400)),
+                        ),
+                      ],
+                    )),
+              ],
+            )),
+          );
+        },
+      ),
     );
   }
 }
